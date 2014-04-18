bea875198d2545e583a299561e1e172947029d08
FrankLin <frank.lin2000@hotmail.com>
2014-04-18 20:11:51 +1000

    add #include "pixelColor.h"

--- pixelColor.c
+++ pixelColor.c
@@ -1,3 +1,5 @@
+ #include "pixelColor.h"
+
 unsigned char stepsToRed (int steps){return steps;}
 unsigned char stepsToBlue (int steps){return steps % 128;}
 unsigned char stepsToGreen (int steps){return steps / 64;}
